d1d0c905d185b0f38fa0ac90d44eb28acdac6a93
jaykumarsonkar28-ux <jaykumarsonkar28@gmail.com>
2026-05-27 18:49:39 +0530

    Update Share.js

--- models/Share.js
+++ models/Share.js
@@ -22,5 +22,20 @@ const shareSchema = new mongoose.Schema({
 // Indexing taaki analytics/reports nikalna aasan ho
 shareSchema.index({ videoId: 1 });
 shareSchema.index({ user: 1 });
+// models/Share.js ke andar niche yeh add karein:
+
+shareSchema.post('save', async function (doc, next) {
+  try {
+    // 'doc' ka matlab hai jo share abhi-abhi save hua hai
+    // Hum automatically uski videoId nikal kar count badha rahe hain
+    await mongoose.model('Video').findByIdAndUpdate(doc.videoId, {
+      $inc: { shares: 1 }
+    });
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
 
 module.exports = mongoose.model('Share', shareSchema);
